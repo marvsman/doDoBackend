f6247a4ecd61bf865c0284d2ec5606f1e42183e7
Marvin Samouelian <samouelian@raceresult.com>
2023-02-22 19:24:45 +0100

    [Testing]: testing different events

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
@@ -13,9 +14,56 @@ import (
 
 var log = logrus.New()
 
+const TELEGRAM_TOKEN = "6043369518:AAHGeyqy3BmU8buWR38ACj6YzkqwHMOfyXg"
+const TELEGRAM_CHAT_ID int64 = -821408765
+
 func main() {
 	setupLogging()
+	bot, err := initTelegram()
+	if err != nil {
+		log.Fatal(err)
+	}
 	app := initPocketbase()
+
+	// check after a record updated
+	app.OnRecordAfterUpdateRequest().Add(func(e *core.RecordUpdateEvent) error {
+		msg := tg.NewMessage(TELEGRAM_CHAT_ID, "OnRecordAfterUpdateRequest")
+		_, err = bot.Send(msg)
+		if err != nil {
+			log.Error(err)
+		}
+
+		log.Println(e.Record)
+
+		return nil
+	})
+
+	// check before the mail was sent
+	app.OnMailerBeforeRecordVerificationSend().Add(func(e *core.MailerRecordEvent) error {
+		msg := tg.NewMessage(TELEGRAM_CHAT_ID, "BEFORE SEND")
+		_, err = bot.Send(msg)
+		if err != nil {
+			log.Error(err)
+		}
+		log.Println("BEFORE SEND")
+		log.Println(e)
+
+		return nil
+	})
+
+	// check after the mail was sent
+	app.OnMailerAfterRecordVerificationSend().Add(func(e *core.MailerRecordEvent) error {
+		msg := tg.NewMessage(TELEGRAM_CHAT_ID, "AFTER SEND")
+		_, err = bot.Send(msg)
+		if err != nil {
+			log.Error(err)
+		}
+
+		log.Println("AFTER SEND")
+		log.Println(e)
+
+		return nil
+	})
 
 	// create default userSettings entry for new users
 	app.OnRecordBeforeConfirmVerificationRequest().Add(func(e *core.RecordConfirmVerificationEvent) error {
@@ -51,7 +99,17 @@ func main() {
 
 func setupLogging() {
 	log.SetOutput(os.Stdout)
-	log.SetLevel(logrus.InfoLevel)
+	log.SetLevel(logrus.DebugLevel)
+}
+
+func initTelegram() (*tg.BotAPI, error) {
+	bot, err := tg.NewBotAPI(TELEGRAM_TOKEN)
+	if err != nil {
+		return nil, err
+	}
+	bot.Debug = true
+
+	return bot, nil
 }
 
 func initPocketbase() *pocketbase.PocketBase {
